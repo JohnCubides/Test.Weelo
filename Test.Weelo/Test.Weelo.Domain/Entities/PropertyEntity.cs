2beffa396f0e27752e940e28654170162896ef28
John Cubides <john.cubides@bitsamericas.com>
2022-01-06 23:34:45 -0500

    Database model created and initial migrations generated

---
+++ Test.Weelo/Test.Weelo.Domain/Entities/PropertyEntity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Test.Weelo.Domain.Entities
+{
+    [Table("Property")]
+    public class PropertyEntity
+    {
+        [Key]
+        public int IdProperty { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public string CodeInternal { get; set; }
+
+        public int Year { get; set; }
+
+        [ForeignKey("IdOwner")]
+        public OwnerEntity Owner { get; set; }
+        public int IdOwner { get; set; }
+
+        public List<PropertyTraceEntity> PropertyTraces { get; set; }
+
+        public List<PropertyImageEntity> PropertyImages { get; set; }
+
+    }
+}
