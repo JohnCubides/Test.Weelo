6e3666f6bac98f8a6f0cc52640cd8a57777f59ed
John Cubides <john.cubides@bitsamericas.com>
2022-01-08 03:01:21 -0500

    Update Migrations include attribute file in PropertyImage table

---
+++ Test.Weelo/Test.Weelo.Persistence/Migrations/Application/20220108080016_Update-propertyImage-Table.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test.Weelo.Persistence.Migrations.Application
+{
+    public partial class UpdatepropertyImageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "file",
+                table: "PropertyImage",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "file",
+                table: "PropertyImage");
+        }
+    }
+}
